694ff910d47c0f961b3269c188ce975a4a410638
maerla7 <majaerso@gmail.com>
2026-09-07 18:55:43 +0200

    tema 2 oppgave 2 + navneendirnger i filene på vs og git

--- tema-1-oppgaver/oppgave2.php
+++ tema-1-oppgaver/oppgave2.php
@@ -6,5 +6,5 @@
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"]; 
 	
-  print ("God dag $fornavn $etternavn , du er $alder år gammel! <br />");  
+  print ("God dag $fornavn $etternavn , du er $alder år gammel!");  
 ?>
