74b89679a99372f348ee3f5ad264ac1c2592adf9
maerla7 <majaerso@gmail.com>
2026-09-08 15:06:03 +0200

    oppgave 4 html og php

--- tema-2-oppgaver/oppgave4.php
+++ tema-2-oppgaver/oppgave4.php
@@ -4,8 +4,8 @@
 /*      og et svar på spørsmålet "Hvor gammel er du ?"
 /*    Programmet sjekker hva som er svart på spørsmålene og skriver ut en passende melding*/
 
-$gift=isset ($_POST) ["gift"] ? strotolower ($_POST["gift"]) : "";
-$barn=isset ($_POST) ["barn"] ? strotolower ($_POST["barn"]) : "";
+$gift=isset ($_POST) ["gift"] ? strotolower ($_POST["gift"]);
+$barn=isset ($_POST) ["barn"] ? strotolower ($_POST["barn"]);
 
 if (!$gift or !$barn)
   {print ("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene");}
