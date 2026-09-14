d03420b65ed50651a4664cd51098c920f12c791c
maerla7 <majaerso@gmail.com>
2026-09-14 13:50:48 +0200

    oppgave 4 php fil

--- tema-2-oppgaver/oppgave4.php
+++ tema-2-oppgaver/oppgave4.php
@@ -4,30 +4,30 @@
 /*      og et svar på spørsmålet "Hvor gammel er du ?"
 /*    Programmet sjekker hva som er svart på spørsmålene og skriver ut en passende melding*/
 
-$gift=isset ($_POST) ["gift"] strotolower ($_POST["gift"]);
-$barn=isset ($_POST) ["barn"] strotolower ($_POST["barn"]);
+$gift=$_POST ["gift"];
+$barn=$_POST ["barn"];
 
 if (!$gift or !$barn)
   {
     print ("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene");
     }
 
-else if ($gift == "ja" && $barn == "ja")
+else if ($gift == "ja" or $gift == "JA" or $gift == "Ja" && $barn == "ja" or $barn == "JA" or $barn == "Ja")
   {
     print ("Gratulerer, du har barn og er gift!");
 }
 
-else if ($gift == "nei" && $barn == "nei")
+else if ($gift == "nei" or $gift == "NEI" or $gift == "Nei" && $barn == "nei" or $barn == "NEI" or $barn == "Nei")
   {
     print ("Uffda, du har ingen barn og er ikke gift:(");
 }
 
-else if ($gift == "nei" && $barn == "ja" )
+else if ($gift == "nei" or $gift == "NEI" or $gift == "Nei" && $barn == "ja" or $barn == "JA" or $barn == "Ja" )
   {
     print ("Jaja, du har i det minste barn:)");
 }
 
-else if ($gift == "ja" && $barn == "nei")
+else if ($gift == "ja" or $gift == "JA" or $gift == "Ja" && $barn == "nei" or $barn == "NEI" or $barn == "Nei")
 {
   print ("Jaja, du er i det minste gift:)");
 }
