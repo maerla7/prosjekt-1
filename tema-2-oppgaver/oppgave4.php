8038f4b24b3a3ba5fa8d419350b208118e76ad79
maerla7 <majaerso@gmail.com>
2026-09-14 13:38:06 +0200

    oppgave 4 php fil

--- tema-2-oppgaver/oppgave4.php
+++ tema-2-oppgaver/oppgave4.php
@@ -4,27 +4,37 @@
 /*      og et svar på spørsmålet "Hvor gammel er du ?"
 /*    Programmet sjekker hva som er svart på spørsmålene og skriver ut en passende melding*/
 
-$gift=isset ($_POST) ["gift"] ? strotolower ($_POST["gift"]);
-$barn=isset ($_POST) ["barn"] ? strotolower ($_POST["barn"]);
+$gift=isset ($_POST) ["gift"] strotolower ($_POST["gift"]);
+$barn=isset ($_POST) ["barn"] strotolower ($_POST["barn"]);
 
 if (!$gift or !$barn)
-  {print ("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene");}
+  {
+    print ("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene");
+    }
 
 else if ($gift == "ja" && $barn == "ja")
-  {print ("Gratulerer, du har barn og er gift!");}
+  {
+    print ("Gratulerer, du har barn og er gift!");
+}
 
 else if ($gift == "nei" && $barn == "nei")
-  {print ("Uffda, du har ingen barn og er ikke gift:(");}
+  {
+    print ("Uffda, du har ingen barn og er ikke gift:(");
+}
 
 else if ($gift == "nei" && $barn == "ja" )
-  {print ("Jaja, du har i det minste barn:)");}
+  {
+    print ("Jaja, du har i det minste barn:)");
+}
 
 else if ($gift == "ja" && $barn == "nei")
-{print ("Jaja, du er i det minste gift:)");}
+{
+  print ("Jaja, du er i det minste gift:)");
+}
 
 else 
-  {print ("Ingen av sp&oslash;rsm&aring;lene har gyldig svar. Pr&oslash;v igjen!");}
-
-
+  {
+    print ("Ingen av sp&oslash;rsm&aring;lene har gyldig svar. Pr&oslash;v igjen!");
+}
 
 ?>
