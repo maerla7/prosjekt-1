93878e4abfbb9250941572d1fc29fc729e64d7e6
maerla7 <majaerso@gmail.com>
2026-09-18 08:55:32 +0200

    oppgave 5 php

--- tema-2-oppgaver/oppgave5.php
+++ tema-2-oppgaver/oppgave5.php
@@ -6,25 +6,25 @@ $tall3=$_POST ["tall3"];
 if ($tall3 == 1)
   {$sum=$tall1+$tall2;
   
-    print ("$sum");
+    print ("tall1 er $tall1 og tall2 er $tall2. Hvis vi plusser tall1 med $tall2 får vi $sum");
   }  
 
 else if ($tall3 == 2)
  {
   $sum=$tall1-$tall2;
-    print ("$sum");
+    print ("tall1 er $tall1 og tall2 er $tall2. Hvis vi trekker fra $tall2 fra $tall1 får vi $sum");
  } 
 
 else if ($tall3==3)
   {
     $sum=$tall1*$tall2;
-  print ("$sum");
+  print ("tall1 er $tall1 og tall2 er $tall2. Hvis vi ganger $tal1 med $tall2 får vi $sum");
   }
 
   else if ($tall3==4)
     {
       $sum=$tall1/$tall2;
-      print ("$sum");
+      print ("tall1 er $tall1 og tall2 er $tall2. Hvis vi deler $tal1 med $tall2 får vi $sum");
     }
 
     else 
