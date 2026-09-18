56453f49a4596b6baa6423713175a491c3238d81
maerla7 <majaerso@gmail.com>
2026-09-18 08:59:07 +0200

    oppgave 5 php

--- tema-2-oppgaver/oppgave5.php
+++ tema-2-oppgaver/oppgave5.php
@@ -24,7 +24,7 @@ else if ($tall3==3)
   else if ($tall3==4)
     {
       $sum=$tall1/$tall2;
-      print ("tall1 er $tall1 og tall2 er $tall2. Hvis vi deler $tal1 med $tall2 får vi $sum");
+      print ("tall1 er $tall1 og tall2 er $tall2. Hvis vi deler $tall1 med $tall2 får vi $sum");
     }
 
     else 
