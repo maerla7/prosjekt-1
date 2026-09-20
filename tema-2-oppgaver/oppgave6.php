f0cc43c709ddf0f3034d1ee5e90ee2bba4dd3a21
maerla7 <majaerso@gmail.com>
2026-09-20 16:49:53 +0200

    php fil 6

--- tema-2-oppgaver/oppgave6.php
+++ tema-2-oppgaver/oppgave6.php
@@ -3,19 +3,27 @@ $tall1=$_POST ["tall1"];
 $tall2=$_POST ["tall2"];
  if ($tall1==$tall2)
   {
-print ("Tallene er like");
-}
+    print ("Tall 1 er $tall1 og tall 2 er $tall2. Tallene er like");
+  }
  
 else if ($tall1!=$tall2)
-  {print ("Tallene er ikke like");}
+  {
+    print ("Tall 1 er $tall1 og tall 2 er $tall2. Tallene er ikke like");
+  }
 
 else if ($tall1 > $tall2)
-  {print ("Tall1 er mindre enn tall2");}
+  {
+    print ("Tall 1 er $tall1 og tall 2 er $tall2. Tall1 er mindre enn tall2");
+  }
 
 else if ($tall1 < $tall2)
-{print ("Tall1 er st$oslash;rre enn tall2");}
+{
+  print ("Tall 1 er $tall1 og tall 2 er $tall2. Tall1 er st$oslash;rre enn tall2");
+}
 
 else 
-  {print ("Det er ikke mulig $aring; avgj$oslash;re om tallene er like eller ikke:(");}
+  {
+    print ("Det er ikke mulig $aring; avgj$oslash;re om tallene er like eller ikke:(");
+  }
 
 ?>
