0114138988d11a3087a4851559754bfa7bd42219
maerla7 <majaerso@gmail.com>
2026-09-20 15:55:42 +0200

    oppgve 6 html og php fil

---
+++ tema-2-oppgaver/oppgave6.php
@@ -0,0 +1,12 @@
+<?php
+$tall1=$_POST ["tall1"];
+$tall2=$_POST ["tall2"];
+ if ($tall1==$tall2)
+  {
+print ("Tallene er like");
+}
+ 
+else if ($tall1!=$tall2)
+  {print ("Tallene er ikke like, tall1 mangler");}
+
+?>
