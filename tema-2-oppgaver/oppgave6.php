ef16b1f7706d6b76951b606366c7e39a28f8adb6
maerla7 <majaerso@gmail.com>
2026-09-20 17:09:21 +0200

    php fil 6

--- tema-2-oppgaver/oppgave6.php
+++ tema-2-oppgaver/oppgave6.php
@@ -5,12 +5,12 @@ $tall2=$_POST ["tall2"];
 
 if ($tall1>$tall2)
   {
-    print ("Tall 1 er $tall1 og tall 2 er $tall2. Tall1 er mindre enn tall2");
+    print ("Tall 1 er $tall1 og tall 2 er $tall2. Tall1 er større enn tall2");
   }
 
 else if ($tall1<$tall2)
 {
-  print ("Tall 1 er $tall1 og tall 2 er $tall2. Tall1 er større enn tall2");
+  print ("Tall 1 er $tall1 og tall 2 er $tall2. Tall1 er mindre enn tall2");
 }
 
 else 
