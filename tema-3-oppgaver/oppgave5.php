32c66cca60ddeed00a9c182162213b572aa51d07
maerla7 <majaerso@gmail.com>
2026-09-21 22:38:40 +0200

    oppgave 5 html og php

---
+++ tema-3-oppgaver/oppgave5.php
@@ -0,0 +1,19 @@
+<?php
+
+$tekst="Maja;aka mj;it og ledelse;noobie";
+
+$del=explode (";", $tekst);
+
+$fornavn=$del[0];
+$kallenavn=$del[1];
+$studie=$del[2];
+$nivå=$del[3];
+
+print ("Hele settningen er: $tekst <br/> <br/>");
+
+print ("Fornavnet er: $fornavn <br/>");
+print ("Kallenavnet til $fornavn er: $kallenavn <br/>");
+print ("Studiet $kallenavn er: $studie <br/>");
+print ("Nivået på studiet til $kallenavn er: $nivå <br/>");
+
+?>
