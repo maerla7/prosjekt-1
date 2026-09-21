ad92af5322a84d57fe9467be9ea714cc5fbb2fcf
maerla7 <majaerso@gmail.com>
2026-09-21 21:52:18 +0200

    oppgave 7 php

---
+++ tema-3-oppgaver/oppgave2.php
@@ -0,0 +1,21 @@
+<?php
+/*
+/*    Programmet skriver ut tallene fra 1 til 10 på hver sin linje
+/*    Eksempelet viser bruk av while-setning
+*/
+
+$tall=1;
+
+while ($tall<=10)
+  {
+    print ("$tall <br/>");
+    $tall++;
+  }
+
+  /*
+startverdi: $tall=1 
+betingelse: $tall<=10
+økning: $tall++ (dette er det samme som $tall=$tall+1)
+*/
+
+?>
