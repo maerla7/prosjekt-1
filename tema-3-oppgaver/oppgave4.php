9cab7bc6ed41347610de2bb658d260e222e37b8c
maerla7 <majaerso@gmail.com>
2026-09-21 22:23:05 +0200

    oppgave 4 html og php

---
+++ tema-3-oppgaver/oppgave4.php
@@ -0,0 +1,15 @@
+<?php
+/*
+/*    Programmet legger inn antall dager i hver måned i et array
+/*    Programmet skriver ut antall dager i hver måned
+*/
+
+$antallDager=array (1=>31,28,31,30,31,30,31,31,30,31,30,31);
+
+print ("Antall dager i september er $antallDager[9] <br/> ");
+
+for ($mnd=1; $mnd<=12; $mnd++)
+  {
+    print ("Antall dager i måned nr $mnd er $antallDager[$mnd] <br/>");
+  }
+?>
